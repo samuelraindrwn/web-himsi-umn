0920f6e1d07f3d15e68581d096db1a17514b3bc1
Samuel Rayy <samueraylovers1@gmail.com>
2024-02-11 20:34:42 +0700

    test

--- src/components/(pages)/home/student-creation/creation-section.tsx
+++ src/components/(pages)/home/student-creation/creation-section.tsx
@@ -2,10 +2,23 @@ export default function HasilKarya() {
   return (
     <>
       <section className="creation-container">
-        <h1>HASIL KARYA MAHASISWA SISTEM INFORMASI</h1>
+        <h1>
+          HASIL KARYA <span className="font-clr-blue">MAHASISWA</span>{" "}
+          <span className="font-clr-orange">SISTEM INFORMASI</span>
+        </h1>
         <div className="creation-wrapper">
           <div id="uiux">
-            <video autoPlay src="/vids/uiux.mp4"></video>
+            <video autoPlay muted>
+              <source src="/vids/uiux.mp4" type="video/mp4" />
+            </video>
+            <article className="desc">
+              <h2>UI/UX</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+                quis voluptatem est perferendis fuga sapiente natus saepe eos
+                omnis atque maxime expedita reprehenderit aliquam dicta?
+              </p>
+            </article>
           </div>
           <div>DATA VISUALISASI</div>
           <div>WEBSITE</div>

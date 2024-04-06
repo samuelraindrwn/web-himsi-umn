980e350f83fe1ff520fc6a289e2a0f040a78f7b8
Samuel Rayy <samueraylovers1@gmail.com>
2024-04-06 21:09:59 +0700

    BIMSI SECTION

--- src/components/(pages)/home/bimsi-section/bimsi-section.tsx
+++ src/components/(pages)/home/bimsi-section/bimsi-section.tsx
@@ -2,7 +2,20 @@ export default function BimsiSection() {
   return (
     <>
       <section id="bimsi" className="bimsi-container">
-        <div className="bimsi-wrapper">bimsi</div>
+        <div className="bimsi-wrapper">
+          <div className="bimsi-body">
+            <h1>BIMBEL HIMSI</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+              perspiciatis sapiente assumenda! Quos vero consequatur tenetur
+              placeat dolor quod hic, quidem magnam optio modi quo quaerat
+              voluptatem soluta cum porro? Fugiat sapiente est delectus
+              veritatis laboriosam facere dolorum et deleniti, illo commodi
+              distinctio, recusandae nesciunt ipsa a in rem sequi.
+            </p>
+            <button className="btn-style-1">JOIN NOW</button>
+          </div>
+        </div>
       </section>
     </>
   );
